f2858e6c4ded7b56971656d1075ba8c3f38415aa
Aaron N. Rudolph <aaron.rudolph@sap.com>
2017-04-10 09:41:03 -0500

    Initial Commit

---
+++ Barcode Scanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Barcode Scanner
+//
+//  Created by Rudolph, Aaron on 4/10/17.
+//  Copyright © 2017 Aaron Rudolph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Barcode Scanner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Barcode Scanner
+//
+//  Created by Rudolph, Aaron on 4/10/17.
+//  Copyright © 2017 Aaron Rudolph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
